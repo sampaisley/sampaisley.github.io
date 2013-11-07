e35f79c9fe811dab2b541db86b4f68bfe32fa8e0
paisleysam <ppetalski@yahoo.com>
2013-11-07 18:56:02 +0000

    googlSpread and form

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -6,6 +6,14 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
 
-  }]);
+        .controller('MyCtrl2', function ($scope, $http) {     ///--- PAGE 2 CONTROL ---
+        $http.get('https://spreadsheets.google.com/feeds/list/0AhZ7a-ySJXJTdE1mNDJPSTEwY2c0TlhqWkg2ZFFYd3c/od6/public/values?alt=json', { cache: true }).success(function(data) {
+                console.log(data.feed.entry);
+            $scope.d= data.feed.entry;
+
+        });
+
+
+
+  });
