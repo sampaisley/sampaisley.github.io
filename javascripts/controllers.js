d316a5c367b659eb097d4298fb907d8a4dbb7042
paisleysam <ppetalski@yahoo.com>
2013-11-09 12:13:17 +0000

    Images added

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 /* Controllers */
 
@@ -8,12 +8,29 @@ angular.module('myApp.controllers', []).
   }])
 
         .controller('MyCtrl2', function ($scope, $http) {     ///--- PAGE 2 CONTROL ---
+        // get the google sread sheet
+
+
         $http.get('https://spreadsheets.google.com/feeds/list/0AhZ7a-ySJXJTdE1mNDJPSTEwY2c0TlhqWkg2ZFFYd3c/od6/public/values?alt=json', { cache: true }).success(function(data) {
-                console.log(data.feed.entry);
+               // console.log(data.feed.entry);
             $scope.d= data.feed.entry;
 
         });
 
+        // get img from flickr
+        $scope.set =  "72157635342624133";  // sea as default set
+        function getAndShow(){
+
+ var url="http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&photoset_id=72157635342624133&api_key=2fdc79859cd894e55ee6fb2d0a4e6acf&user_id=100786833@N08&format=json&callback=foo";
+
+            $http.jsonp(url);
+
+            jsonFlickrApi = function (result) {
+                $scope.pset = result.photoset.photo[0];
+
+            }
+        }
+        getAndShow()
 
 
   });
