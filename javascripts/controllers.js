69b01e38656f23979b096bee93e755978b2e90e8
paisleysam <ppetalski@yahoo.com>
2013-11-18 12:49:55 +0000

    html filter added

--- javascripts/controllers.js
+++ javascripts/controllers.js
@@ -4,22 +4,20 @@
 
 angular.module('myApp.controllers', []).
     controller('MyCtrl1', function ($scope, $sce) {
-        $scope.opts = [
-            {value: 111, text: '<b>1st</b>' },
-            {value: 222, text: '<i>2nd</i>' }
-        ];
-        $scope.html = '<ul><li>render me please</li></ul>';
+
+        $scope.html = '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>';
         $scope.trustedHtml = $sce.trustAsHtml($scope.html);
 
   })
 
-        .controller('MyCtrl2', function ($scope, $http) {     ///--- PAGE 2 CONTROL ---
+        .controller('MyCtrl2', function ($scope, $http, $sce) {     ///--- PAGE 2 CONTROL ---
         // get the google sread sheet
 
 
         $http.get('https://spreadsheets.google.com/feeds/list/0AhZ7a-ySJXJTdE1mNDJPSTEwY2c0TlhqWkg2ZFFYd3c/od6/public/values?alt=json', { cache: true }).success(function(data) {
                // console.log(data.feed.entry);
             $scope.d= data.feed.entry;
+
 
         });
 
--- javascripts/filters.js
+++ javascripts/filters.js
@@ -7,7 +7,7 @@ angular.module('myApp.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]).filter('unsafe', function($sce) {
+  }]).filter('unsafe', function($sce) {    //http://stackoverflow.com/questions/18340872/how-do-you-use-sce-trustashtmlstring-to-replicate-ng-bind-html-unsafe-in-angu
         return function(val) {
             return $sce.trustAsHtml(val);
         };
