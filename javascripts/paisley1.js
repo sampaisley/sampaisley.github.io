1d73024c1187a8af4c90f92c14be4ba8bade8875
paisleysam <ppetalski@yahoo.com>
2013-12-02 18:24:11 +0000

    goog fon & breakpoints

---
+++ javascripts/paisley1.js
@@ -0,0 +1,22 @@
+[
+    {
+        "title": "Food n Stuff",
+        "content": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.",
+        "img": "http://farm4.staticflickr.com/3685/9653233575_537c14744e_q.jpg"
+    },
+    {
+        "title": "Beer & Chips",
+        "content": "Bollox!!!!  semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempu",
+        "img": "http://farm3.staticflickr.com/2833/9653220749_89df91d4aa_m.jpg"
+    },
+    {
+        "title": "Bol - LL - OX",
+        "content": "Is there any knowledge in the world which is so certain that no reasonable man could doubt it? This question, which at first sight might not seem difficult, is really one of the most difficult that can be asked.",
+        "img": "https://dl.dropboxusercontent.com/u/15762817/git-img/screen1.jpg"
+    },
+    {
+        "title": "Even More Bollox",
+        "content": "When we have realized the obstacles in the way of a straightforward and confident answer, we shall be well launched on the study of philosophy—for philosophy is merely the attempt to answer such ultimate questions, not carelessly and dogmatically, as we do in ordinary life and even in the sciences, but critically, after exploring all that makes such questions puzzling, and after realizing all the vagueness and confusion that underlie our ordinary ideas.",
+        "img": "https://dl.dropboxusercontent.com/u/15762817/git-img/screen2.jpg"
+    }
+]
