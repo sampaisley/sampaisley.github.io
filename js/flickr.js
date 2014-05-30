ad7ca2072de28a8ce535aa02f347cf66346b60eb
paisleysam <ppetalski@yahoo.com>
2014-05-30 08:55:32 +0100

    tidy js

--- js/flickr.js
+++ js/flickr.js
@@ -1,11 +1,11 @@
-     var elem = document.querySelector('#draggable');
+     var dragger = document.querySelector('#draggable');
      var strips = document.querySelector('#strips');
      var borderWidth = 10;
      strips.style.borderWidth = borderWidth + "px";
      var theImage = document.querySelector('#theImage');
      var title = document.querySelector('#title');
      var tit;
-     var grid_size = elem.clientWidth;
+     var grid_size = dragger.clientWidth;
      var wide = document.querySelector('#strips').clientWidth;
      var high = document.querySelector('#strips').clientHeight;
      var squaresAccross = wide / grid_size;
@@ -23,7 +23,7 @@
       //add the flickr javascript to the page so it gets executed
       //flickr automatically calls jsonflickrAPI(rsp)
 
-     var script = document.createElement('script');
+     var script = document.createdraggerent('script');
      script.setAttribute('type', 'text/javascript');
      script.setAttribute('src', document.flickrURL);
      document.head.appendChild(script);
@@ -50,17 +50,17 @@
      }
 
      function reSetDragger(h) {
-       if (h < oldHeight && parseInt(elem.style.top) + 1.5 * grid_size > high) {
+       if (h < oldHeight && parseInt(dragger.style.top) + 1.5 * grid_size > high) {
          var down = Math.ceil(h / squaresAccross - 1);
 
-         elem.style.top = down * grid_size + "px";
+         dragger.style.top = down * grid_size + "px";
 
-         var t = parseInt(elem.style.top) / grid_size;
-         var l = parseInt(elem.style.left) / grid_size - 1;
-         //elem.innerHTML =(t+l) || 1;
+         var t = parseInt(dragger.style.top) / grid_size;
+         var l = parseInt(dragger.style.left) / grid_size - 1;
+         //dragger.innerHTML =(t+l) || 1;
           _X = l+2;
           _Y = t*squaresAccross ;
-         elem.innerHTML = t * squaresAccross + l + 2;
+         dragger.innerHTML = t * squaresAccross + l + 2;
          setImage();
          setTitle();
        }
@@ -70,20 +70,20 @@
 
      function setDivHeight(l) {
        var w = 3,
-         h;
-       if (l % 2 !== 0 && l <= 10) {
-         h = Math.round(l / 3);
-
-       } else if (l % 2 !== 0 && l > 10) {
          h = Math.ceil(l / 3);
-
-       } else if (l % 2 === 0 && l <= 10) {
-         h = l / 3;
-
-       } else if (l % 2 === 0 && l > 10) {
-         h = Math.round(l / 2);
-
-       }
+      //  if (l % 2 !== 0 && l <= 10) {
+      //    h = Math.ceil(l / 3);
+       //
+      //  } else if (l % 2 !== 0 && l > 10) {
+      //    h = Math.ceil(l / 3);
+       //
+      //  } else if (l % 2 === 0 && l <= 10) {
+      //    h = l / 3;
+       //
+      //  } else if (l % 2 === 0 && l > 10) {
+      //    h = Math.ceil(l / 3);console.log('obj');
+       //
+      //  }
 
        strips.style.height = h * grid_size + (2 * borderWidth) + 'px';
        strips.style.width = w * grid_size + (2 * borderWidth) + 'px';
@@ -118,7 +118,7 @@
 
 
 
-     var draggie = new Draggabilly(elem, {
+     var draggie = new Draggabilly(dragger, {
        grid: [grid_size, grid_size],
        containment: true
      });
@@ -127,7 +127,7 @@
      function onDragMove(instance) {
        _X = instance.position.x / grid_size + 1;
        _Y = Math.floor(instance.position.y / grid_size * squaresAccross);
-       elem.innerHTML = _Y + _X;
+       dragger.innerHTML = _Y + _X;
      }
 
      function onDragEnd() {
@@ -157,6 +157,6 @@
        sea.onclick = function() {
          changeSet(this.getAttribute('id'));
      };
-     elem.ondblclick = function() {
+     dragger.ondblclick = function() {
        alert("Don't click it dumbo, drag it");
      };
