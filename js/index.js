ca3e8e7bc6615083e7a63d88ce2f07c5b3898565
paisleysam <ppetalski@yahoo.com>
2014-09-07 12:26:32 +0100

    tidy up js

--- js/index.js
+++ js/index.js
@@ -12,16 +12,17 @@ $( document ).ready(function() {
     }
 
 
-  function go(e){
 
-    var msg = $("#msg").val();
-    var phone = $("#phone").val();
-    var email = $("#email").val();
-    if(!msg || !phone || !email){
+  function go(){
+
+    var msg = $("#msg").val(),
+    phone = $("#phone").val(),
+     email = $("#email").val();
+    if(!msg || !phone || !email || !isValidEmailAddress(email)){
       $("#error").text("Check Your Inputs.");
       return;
     }else{
-      $("#contact").addClass('green').text("Message Sent").fadeOut(3000,function(){$(this).text("Contact").removeClass("green").fadeIn()});
+      $("#contact").addClass('green').text("Message Sent").fadeOut(3000,function(){$(this).text("Contact").removeClass("green").fadeIn();});
       clear_form($("#form"));
       $("#form").toggleClass("vis");
       $('h4,h3,p,ul,li').toggleClass("fade");
@@ -32,7 +33,7 @@ $( document ).ready(function() {
       //url: "http://cmh.netne.net/mail/mail.php",
       url:"http://paisley.orgfree.com/mail.php",
       data: { msg: msg, phone: phone, email: email }
-    })
+    });
 
   }
 
@@ -44,9 +45,13 @@ $( document ).ready(function() {
   );
   $("#contact, #close").click(
     function(){
-      $("#error").text("")
+      $("#error").text("");
       $("#form").toggleClass("vis");
       $('h4,h3,p,ul,li').toggleClass("fade");
     }
-  )
+  );
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+}
 });
