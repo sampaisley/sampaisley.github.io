b4ffdedf7166ce76763be0951335511f22cd3588
paisleysam <ppetalski@yahoo.com>
2014-09-08 16:25:50 +0100

    prevent dbl click

--- js/index.js
+++ js/index.js
@@ -46,13 +46,20 @@ $( document ).ready(function() {
     }
   );
   $("#contact, #close").click(
+
     function(){
-      $("#error").text("").removeClass("actif");
-      clear_form($("#form"));
-      $("#form").fadeToggle( "fast").toggleClass("vis");
-      $('h4,h3,p,ul,li').toggleClass("fade");
-    }
-  );
+      if ($("#form:animated").length)
+        {
+          return false;         // prevent double clicking during fade
+        }
+        $("#error").text("").removeClass("actif");
+        clear_form($("#form"));
+        $("#form").fadeToggle( "slow").toggleClass("vis");
+        $('h4,h3,p,ul,li').toggleClass("fade");
+      }
+    );
+
+  
 function isValidEmailAddress(emailAddress) {
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
     return pattern.test(emailAddress);
