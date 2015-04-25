d6301ce6b1649d21935ba79462a6a1132fd11ef7
paisleysam <ppetalski@yahoo.com>
2015-04-25 14:04:15 +0100

    re-enable url

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
       type: "POST",
       //url: "http://cmh.netne.net/mail/mail.php",
       //url:"http://paisley.orgfree.com/mail.php",
-      //  url: "http://cmh.netne.net/mail.php",
+      url: "http://cmh.netne.net/mail.php",
       data: {
         msg: msg,
         phone: phone,
