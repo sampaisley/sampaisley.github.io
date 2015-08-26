bd06a136a872b396e308bafd6612e75eda377209
paisleysam <ppetalski@yahoo.com>
2015-08-26 17:27:05 +0100

    byte host

--- js/one-page.js
+++ js/one-page.js
@@ -62,8 +62,8 @@ $(document).ready(function() {
 	function sendMessage() {
 		$.ajax({
 			type: "POST",
-			url: "http://cmh.netne.net/mail.php",
-			//url: "http://petalski.byethost7.com/mail.php",
+			//url: "http://cmh.netne.net/mail.php",
+			url: "http://petalski.byethost7.com/mail.php",
 			data: {
 				msg: msg,
 				phone: phone,
