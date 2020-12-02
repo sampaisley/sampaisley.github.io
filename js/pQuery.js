e507e4d45cd0afa27d33d3b758ea28997b4fc4df
sampaisley <sampaisley@hotmail.com>
2020-12-02 14:31:48 +0000

    pQuery.js

---
+++ js/pQuery.js
@@ -0,0 +1,147 @@
+function $(selector) {
+	const el = document.querySelectorAll(selector);
+	let n = null;
+
+
+	if (!el) return;
+
+	return {
+		el,
+		n,
+
+
+		eq(eq) {
+			n = eq;
+			return this;
+		},
+
+
+
+		text(tx) {
+
+			if (tx) {
+				for (let i = 0; i < el.length; i++) {
+
+					if ((n || n === 0) && n !== i) continue;
+					el[i].innerHTML = tx;
+				}
+
+				return this;
+			}
+
+			return el[0].innerHTML;
+		},
+
+
+
+		putClass(cl) {
+
+			if (cl && !cl.trim()) return this; // can't trim empty space
+			cl = cl.trim().split(' ');
+			el.forEach((item, index) => {
+
+				for (let j = 0; j < cl.length; j++) {
+					if ((n || n === 0) && n !== index) continue;
+					item.classList.add(cl[j]);
+				}
+			});
+			return this;
+		},
+
+
+
+
+		takeClass(cl) {
+
+			if (cl && !cl.trim()) return this; // can't trim empty space
+			cl = cl.trim().split(' ');
+			el.forEach(function (item, index) {
+				for (let j = 0; j < cl.length; j++) {
+					if ((n || n === 0) && n !== index) continue;
+
+					item.classList.remove(cl[j]);
+				}
+			}); // pass 'this' to .forEach loop
+			return this;
+		},
+
+
+
+		togClass(cl) {
+
+			if (cl && !cl.trim()) return this; // can't trim empty space
+			cl = cl.trim().split(' ');
+			el.forEach(function (item, index) {
+				for (let j = 0; j < cl.length; j++) {
+					if ((n || n === 0) && n !== index) continue;
+					item.classList.toggle(cl[j]);
+				}
+			});
+			return this;
+		},
+
+
+
+		attribute(att, str) {
+			if (str) {
+				for(let i=0; i< el.length; i++) {
+						if ((n || n === 0) && n !== i) continue;
+					el[i].setAttribute(att, str);
+				}
+				return this;
+			}
+			return el[0].getAttribute(att);
+
+		},
+
+
+
+		val(v) {
+
+			if (v) {
+				el.forEach(function (item) {
+					item.value = v;
+				});
+				return this;
+			}
+			return el[0].value;
+
+		},
+
+
+
+		focus() {
+			el[0].focus();
+			return this;
+		},
+
+
+		scrollToView(bool) {
+			el[0].scrollIntoView(bool);
+			return this;
+		},
+
+
+		hasClass(cl) {
+			return el[0].classList.contains(cl);
+		},
+
+
+		fuckOff() {
+			el.forEach(function (item) {
+				item.remove();
+			});
+		},
+
+
+
+		on(event, callback) {
+			el.forEach(function (item) {
+				item.addEventListener(event, callback, false);
+			});
+			return this;
+		},
+
+
+	};
+}
