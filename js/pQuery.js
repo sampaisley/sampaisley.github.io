2b06d833db4cdcb9f38cec969a9944a8da35f80d
sampaisley <sampaisley@hotmail.com>
2021-01-02 15:30:41 +0000

    fade callback

--- js/pQuery.js
+++ js/pQuery.js
@@ -28,276 +28,283 @@ function $(selector, indx) {
 
     eq(eq, amount = 1) {
 
-        el = Array.from(els); // re-set el
-        if (eq >= 0 && eq <= el.length && eq !== null) {
-          indx = eq;
-          //el = [els[indx]];
-          el = (el.splice(indx, amount));
+      el = Array.from(els); // re-set el
+      if (eq >= 0 && eq <= el.length && eq !== null) {
+        indx = eq;
+        //el = [els[indx]];
+        el = (el.splice(indx, amount));
 
-        }
-        return this;
-      },
-
-
-
-      text(newText) {
-
-        if (newText) {
-          for (let i = 0; i < el.length; i++) {
-
-            el[i].innerHTML = newText;
-          }
-
-          return this;
-        }
-
-        return el[0].innerHTML;
-      },
+      }
+      return this;
+    },
 
 
 
-      putClass(cl) {
-        if (cl && !cl.trim()) return this; // can't trim empty space
+    text(newText) {
 
-        cl = cl.trim().split(' ');
-
-        el.forEach((item) => {
-
-          item.classList.add(...cl);
-
-        });
-        return this;
-      },
-
-
-
-      takeClass(cl) {
-        if (cl && !cl.trim()) return this;
-
-        cl = cl.trim().split(' ');
-
-        el.forEach((item) => {
-
-          item.classList.remove(...cl);
-
-        });
-        return this;
-      },
-
-
-
-      togClass(cl) {
-
-        if (cl && !cl.trim()) return this;
-        cl = cl.trim().split(' ');
-
-
-        for (let j = 0; j < el.length; j++) {
-          el[j].classList.toggle(cl[j]);
-        }
-
-        return this;
-      },
-
-
-
-      attribute(att, str) {
-        if (str) {
-          for (let i = 0; i < el.length; i++) {
-
-            el[i].setAttribute(att, str);
-          }
-          return this;
-        }
-        return el[0].getAttribute(att);
-
-      },
-
-      checked() {
-
-
-        return el[0].checked;
-
-      },
-
-
-
-      removeAtt(att) {
-
+      if (newText) {
         for (let i = 0; i < el.length; i++) {
 
-          el[i].removeAttribute(att);
+          el[i].innerHTML = newText;
+        }
 
+        return this;
+      }
+
+      return el[0].innerHTML;
+    },
+
+
+
+    putClass(cl) {
+      if (cl && !cl.trim()) return this; // can't trim empty space
+
+      cl = cl.trim().split(' ');
+
+      el.forEach((item) => {
+
+        item.classList.add(...cl);
+
+      });
+      return this;
+    },
+
+
+
+    takeClass(cl) {
+      if (cl && !cl.trim()) return this;
+
+      cl = cl.trim().split(' ');
+
+      el.forEach((item) => {
+
+        item.classList.remove(...cl);
+
+      });
+      return this;
+    },
+
+
+
+    togClass(cl) {
+
+      if (cl && !cl.trim()) return this;
+      cl = cl.trim().split(' ');
+
+
+      for (let j = 0; j < el.length; j++) {
+        el[j].classList.toggle(cl[j]);
+      }
+
+      return this;
+    },
+
+
+
+    attribute(att, str) {
+      if (str) {
+        for (let i = 0; i < el.length; i++) {
+
+          el[i].setAttribute(att, str);
         }
         return this;
+      }
+      return el[0].getAttribute(att);
 
-      },
+    },
+
+    checked() {
+
+
+      return el[0].checked;
+
+    },
 
 
 
-      selectedIndex(i) {
+    removeAtt(att) {
 
-        if (i || i === 0) {
-          el[0].options.selectedIndex = i;
-          return this;
+      for (let i = 0; i < el.length; i++) {
+
+        el[i].removeAttribute(att);
+
+      }
+      return this;
+
+    },
+
+
+
+    selectedIndex(i) {
+
+      if (i || i === 0) {
+        el[0].options.selectedIndex = i;
+        return this;
+      }
+
+      return el[0].selectedIndex;
+
+    },
+
+    placeholder(t) {
+
+      if (t) {
+        for (let i = 0; i < el.length; i++) {
+
+          el[i].placeholder = t;
         }
+        return this;
+      }
+      return el[0].placeholder;
 
-        return el[0].selectedIndex;
+    },
 
-      },
 
-      placeholder(t) {
 
-        if (t) {
-          for (let i = 0; i < el.length; i++) {
+    val(v) {
 
-            el[i].placeholder = t;
+      if (v || v === '') {
+        el.forEach(function (item) {
+          item.value = v;
+        });
+        return this;
+      }
+      return el[0].value;
+
+    },
+
+
+
+    focus() {
+      el[0].focus();
+      return this;
+    },
+
+
+    blur() {
+
+      el.forEach(function (item) {
+        item.blur();
+      });
+      return this;
+    },
+
+
+    scrollToView(bool) {
+      el[0].scrollIntoView(bool);
+      return this;
+    },
+
+
+    hasClass(cl) {
+      return el[0].classList.contains(cl);
+    },
+
+
+    fuckOff() {
+      el.forEach(function (item) {
+        item.remove();
+      });
+    },
+    
+    
+
+    fade(t = 50, del = true,func=null) {
+
+      el.forEach((item) => {
+
+        let fadeEffect = setInterval(function () {
+          if (!item.style.opacity) {
+            item.style.opacity = 1;
           }
-          return this;
-        }
-        return el[0].placeholder;
-
-      },
-
-
-
-      val(v) {
-
-        if (v || v === '') {
-          el.forEach(function(item) {
-            item.value = v;
-          });
-          return this;
-        }
-        return el[0].value;
-
-      },
-
-
-
-      focus() {
-        el[0].focus();
-        return this;
-      },
-
-
-      blur() {
-
-        el.forEach(function(item) {
-          item.blur();
-        });
-        return this;
-      },
-
-
-      scrollToView(bool) {
-        el[0].scrollIntoView(bool);
-        return this;
-      },
-
-
-      hasClass(cl) {
-        return el[0].classList.contains(cl);
-      },
-
-
-      fuckOff() {
-        el.forEach(function(item) {
-          item.remove();
-        });
-      },
-
-      fade(t = 50, del = true) {
-
-        el.forEach((item) => {
-
-          let fadeEffect = setInterval(function() {
-            if (!item.style.opacity) {
-              item.style.opacity = 1;
+          if (item.style.opacity > 0) {
+            item.style.opacity -= 0.1;
+          } else {
+            clearInterval(fadeEffect);
+            if (del){
+              item.remove();
             }
-            if (item.style.opacity > 0) {
-              item.style.opacity -= 0.1;
-            } else {
-              clearInterval(fadeEffect);
-              if (del)
-                item.remove();
+            if(func){
+              func();
             }
-          }, t);
+              
+          }
+        }, t);
 
-        });
-        return this;
+      });
+      return this;
 
-      },
-
-
-      fadeUp(t = 50) {
-        console.log('234');
-        el.forEach((item) => {
-          let i = 0;
-          let fadeEffect = setInterval(function() {
-
-            if (item.style.opacity < 1) {
-              item.style.opacity = i;
-              i += 0.1;
-              console.log('i', i);
-            } else {
-              clearInterval(fadeEffect);
-
-            }
-          }, t);
-
-        });
-
-        return this;
-
-      },
+    },
 
 
+    fadeUp(t = 50) {
+      console.log('234');
+      el.forEach((item) => {
+        let i = 0;
+        let fadeEffect = setInterval(function () {
 
-      on(event, callback) {
-        el.forEach(function(item) {
-          item.addEventListener(event, callback, false);
-        });
-        return this;
-      },
+          if (item.style.opacity < 1) {
+            item.style.opacity = i;
+            i += 0.1;
+            console.log('i', i);
+          } else {
+            clearInterval(fadeEffect);
+
+          }
+        }, t);
+
+      });
+
+      return this;
+
+    },
 
 
 
-      jaxPost(url, data, call) {
-        let r = new XMLHttpRequest();
-
-        r.open("POST", url, true);
-        r.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        r.onreadystatechange = function() {
-
-          if (r.readyState != 4 || r.status != 200) return;
-          if (call)
-            call(this);
-        };
-
-        r.send(data);
-
-        return this;
-
-      },
+    on(event, callback) {
+      el.forEach(function (item) {
+        item.addEventListener(event, callback, false);
+      });
+      return this;
+    },
 
 
-      jaxGet(url, call) {
-        let r = new XMLHttpRequest();
 
-        r.open("GET", url, true);
-        r.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        r.onreadystatechange = function() {
+    jaxPost(url, data, call) {
+      let r = new XMLHttpRequest();
 
-          if (r.readyState != 4 || r.status != 200) return;
+      r.open("POST", url, true);
+      r.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      r.onreadystatechange = function () {
+
+        if (r.readyState != 4 || r.status != 200) return;
+        if (call)
           call(this);
-        };
+      };
 
-        r.send();
+      r.send(data);
 
-        return this;
+      return this;
 
-      },
+    },
+
+
+    jaxGet(url, call) {
+      let r = new XMLHttpRequest();
+
+      r.open("GET", url, true);
+      r.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      r.onreadystatechange = function () {
+
+        if (r.readyState != 4 || r.status != 200) return;
+        call(this);
+      };
+
+      r.send();
+
+      return this;
+
+    },
 
 
   };
